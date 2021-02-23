48564c9857d110430f82c833b83e3ead41dc337c
David Rieser <david.w.rieser@gmail.com>
2021-02-23 10:49:38 +0100

    Fixed UART-Connection via /dev/pts/0 and implemented State-Machine

--- Makefile
+++ Makefile
@@ -12,6 +12,8 @@ ZEPHYR_HOME=~/zephyrproject/zephyr/
 BUILD_DIR=$(ZEPHYR_HOME)build
 EXEC_OUT=$(ZEPHYR_HOME)/build/zephyr/zephyr.elf
 EXEC_OPTIONS=-attach_uart_cmd="xterm -e screen /dev/pts/4 &"
+TEST_SCRIPT=$(abspath test.py)
+TEST_COMMAND=python3 $(TEST_SCRIPT) /dev/pts/0
 
 all: $(EXEC_OUT)
 	@$(MAKE) -s -C $(ZEPHYR_HOME) build
@@ -22,12 +24,18 @@ $(EXEC_OUT): $(SOURCE_FILES)
 	@cd $(ZEPHYR_HOME) && west build -p auto -b $(BOARD) $(ABS_PRJ_DIR)
 	@echo "west build -p auto -b $(BOARD) $(ABS_PRJ_DIR)"
 
+# Run using "make test -j2"
+test: run python_test
+
 run: $(EXEC_OUT)
 	@echo "Running Project"
 	$(EXEC_OUT) $(EXEC_OPTIONS)
 
-config:
-	@cd $(ZEPHYR_HOME) && west build -t menuconfig
+python_test:
+	$(TEST_COMMAND)
+
+# config:
+# 	@cd $(ZEPHYR_HOME) && west build -t menuconfig
 
 # ------------------------------------------------------------------------------
 
--- code/src/main.c
+++ code/src/main.c
@@ -1,29 +1,82 @@
 
 // int usleep(useconds_t useconds);
 
+// https://github.com/zephyrproject-rtos/zephyr/blob/backport-29181-to-v2.4-branch/include/drivers/uart.h
+//
+/** Console I/O function */
+// int (*poll_in)(const struct device *dev, unsigned char *p_char);
+// void (*poll_out)(const struct device *dev, unsigned char out_char);
+//
+// /** Console I/O function */
+// int (*err_check)(const struct device *dev);
+//
+// /** UART configuration functions */
+// int (*configure)(const struct device *dev,
+// 		 const struct uart_config *cfg);
+// int (*config_get)(const struct device *dev, struct uart_config *cfg);
+
+// https://github.com/zephyrproject-rtos/zephyr/blob/backport-29181-to-v2.4-branch/include/posix/pthread.h
+// https://github.com/zephyrproject-rtos/zephyr/blob/backport-29181-to-v2.4-branch/tests/posix/common/src/pthread.c
+// https://github.com/zephyrproject-rtos/zephyr/blob/backport-29181-to-v2.4-branch/tests/posix/common/src/posix_rwlock.c
+//
+// int pthread_once(pthread_once_t *once, void (*initFunc)(void));
+// void pthread_exit(void *retval);
+// int pthread_join(pthread_t thread, void **status);
+// int pthread_cancel(pthread_t pthread);
+// int pthread_detach(pthread_t thread);
+// int pthread_create(pthread_t *newthread, const pthread_attr_t *attr,
+// 		   void *(*threadroutine)(void *), void *arg);
+// int pthread_setcancelstate(int state, int *oldstate);
+// int pthread_attr_setschedparam(pthread_attr_t *attr,
+// 			       const struct sched_param *schedparam);
+// int pthread_setschedparam(pthread_t pthread, int policy,
+// 			  const struct sched_param *param);
+// int pthread_rwlock_destroy(pthread_rwlock_t *rwlock);
+// int pthread_rwlock_init(pthread_rwlock_t *rwlock,
+// 			const pthread_rwlockattr_t *attr);
+// int pthread_rwlock_rdlock(pthread_rwlock_t *rwlock);
+// int pthread_rwlock_timedrdlock(pthread_rwlock_t *rwlock,
+// 			       const struct timespec *abstime);
+// int pthread_rwlock_timedwrlock(pthread_rwlock_t *rwlock,
+// 			       const struct timespec *abstime);
+// int pthread_rwlock_tryrdlock(pthread_rwlock_t *rwlock);
+// int pthread_rwlock_trywrlock(pthread_rwlock_t *rwlock);
+// int pthread_rwlock_unlock(pthread_rwlock_t *rwlock);
+// int pthread_rwlock_wrlock(pthread_rwlock_t *rwlock);
+// int pthread_key_create(pthread_key_t *key,
+// 		void (*destructor)(void *));
+
 #include <zephyr.h>
 #include <device.h>
 #include <sys/printk.h>
 #include <drivers/uart.h>
 #include <crypto/cipher.h>
 #include <crypto/cipher_structs.h>
+#include <logging/log.h>
+#include <pthread.h>
+#include <mqueue.h>
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #define CRYPTO_DRV_NAME CONFIG_CRYPTO_TINYCRYPT_SHIM_DRV_NAME
 #define UART_DRV_NAME "UART_0"
 
 #define LOG_LEVEL CONFIG_CRYPTO_LOG_LEVEL
-#include <logging/log.h>
 LOG_MODULE_REGISTER(main);
 
-enum states{INIT,IDLE,BUSY,AVAIL,DECRYPT,DLEN,DATA,KEY,IV,OP,OP_KEY,OP_IV,OP_DECRYPT};
+// Create Mutex for locking the Message-Queue
+// PTHREAD_MUTEX_DEFINE(queue_lock);
+
+// 3 Threads + Main-Thread
+#define NUM_THREADS 3
 
 int validate_hw_compatibility(const struct device *dev);
 void cbc_mode(const struct device *dev);
+void state_machine();
 void print_data(
     const char *title,
     const char *formatter,
@@ -31,17 +84,36 @@ void print_data(
     int len
 );
 
+// Declare Enum for State Machine
+enum states{INIT,IDLE,BUSY,AVAIL,ENCRYPT,DECRYPT,DLEN,DATA,KEY,IV,OP,OP_KEY,OP_IV,OP_DECRYPT};
+enum operations{OP_INIT,SET_KEY,SET_IV,OP_ENCRYPT,OP_DECRYPT};
+
+static enum states prog_state = INIT;
+static enum operations prog_operation = OP_INIT;
+
+// Globally declare Devices
+const struct device * uart_dev;
+const struct device * crypto_dev;
+
+#define AES_KEY_LEN 16
+#define AES_IV_LEN 16
+
+uint8_t in_message_queue_pointer = 0;
+char in_message_queue[255];
+uint8_t out_message_queue_pointer = 0;
+char out_message_queue[255];
+
 void main(void) {
 
-	uint8_t rx_buf[10] = {0};
-    uint8_t tx_buf[10] = {0x48,0x61,0x6C,0x6C,0x6F,0x21,0x20,0x20,0x20,0x0A};
+	// uint8_t rx_buf[10] = {0};
+    // uint8_t tx_buf[10] = {0x48,0x61,0x6C,0x6C,0x6F,0x21,0x20,0x20,0x20,0x0A};
 
-	const struct device * uart_dev = device_get_binding(UART_DRV_NAME);
+	uart_dev = device_get_binding(UART_DRV_NAME);
 	if (!uart_dev) {
         LOG_ERR("%s pseudo device not found", UART_DRV_NAME);
         return;
     }
-	const struct device * crypto_dev = device_get_binding(CRYPTO_DRV_NAME);
+	crypto_dev = device_get_binding(CRYPTO_DRV_NAME);
 	if (!crypto_dev) {
         LOG_ERR("%s pseudo device not found", CRYPTO_DRV_NAME);
         return;
@@ -52,27 +124,10 @@ void main(void) {
             return;
     }
 
-	cbc_mode(crypto_dev);
+    // Log Crypto-Action
+	// cbc_mode(crypto_dev);
 
-	while (1) {
-
-        LOG_ERR("Error : %d %d", uart_rx_enable(uart_dev, rx_buf, 10, 50), -ENOTSUP);
-
-		if (uart_rx_enable(uart_dev, rx_buf, 10, 50)){
-            LOG_ERR("Error during UART-Receiving");
-            return;
-        }
-
-		printk("Received data : <>\nSending data back\n", rx_buf);
-
-		if(uart_tx(uart_dev,tx_buf,10,50)) {
-            LOG_ERR("Error during UART-Transmission");
-            return;
-        }
-
-		sleep(1);
-
-	}
+    state_machine();
 
 }
 
@@ -86,6 +141,141 @@ void main(void) {
 // Code
 // https://github.com/zephyrproject-rtos/zephyr/blob/master/include/drivers/uart.h
 // https://github.com/zephyrproject-rtos/zephyr/blob/master/drivers/serial/uart_native_posix.c
+
+void state_machine() {
+
+    unsigned char uart_in = '\0';
+    uint8_t iLauf = 0, len = 0;
+    unsigned char * buffer;
+
+    while (1) {
+
+        if(!uart_poll_in(uart_dev,&uart_in)){
+
+            LOG_INF("Received <%c> = <%d>",uart_in,uart_in);
+
+            switch (prog_state) {
+
+                case INIT:
+
+                    switch (uart_in) {
+                        case 'P':
+                            prog_state = AVAIL;
+                            break;
+                        case 'D':
+                            prog_state = DECRYPT;
+                            break;
+                        case 'K':
+                            prog_state = KEY;
+                            break;
+                        case 'I':
+                            prog_state = IV;
+                            break;
+                        case 'W':
+                            // sleep(10);
+                            prog_state = BUSY;
+                            break;
+                        case '.':
+                            uart_poll_out(uart_dev,'.');
+                        default:
+                            prog_state = INIT;
+                            break;
+                    };
+
+                case DECRYPT:
+                    prog_state = DLEN;
+                    prog_operation = OP_DECRYPT;
+                    break;
+
+                case ENCRYPT:
+                    prog_state = DLEN;
+                    prog_operation = OP_ENCRYPT;
+                    break;
+
+                case DLEN:
+                    prog_state = DATA;
+                    while (1) {
+                        if(!uart_poll_in(uart_dev,&uart_in)){
+                            len = uart_in;
+                            break;
+                        }
+                        buffer = malloc(len);
+                    }
+                    break;
+
+                case KEY:
+                    prog_state = DATA;
+                    prog_operation = SET_KEY;
+                    buffer = malloc(AES_KEY_LEN);
+                    len = AES_KEY_LEN;
+                    break;
+
+                case IV:
+                    prog_state = DATA;
+                    prog_operation = SET_IV;
+                    buffer = malloc(AES_IV_LEN);
+                    len = AES_IV_LEN;
+                    break;
+
+                case DATA:
+                    prog_state = OP;
+                    iLauf = 0;
+                    while (len > iLauf){
+                        if(!uart_poll_in(uart_dev,&uart_in)){
+                            buffer[iLauf++] = uart_in;
+                            break;
+                        }
+                    }
+                    break;
+
+                case OP:
+                    switch (prog_operation){
+                        case SET_KEY:
+                            prog_state = OP_KEY;
+                            break;
+                        case SET_IV:
+                            prog_state = OP_IV;
+                            break;
+                        case OP_DECRYPT:
+                            prog_state = OP_DECRYPT;
+                            break;
+                        case OP_ENCRYPT:
+                            prog_state = OP_ENCRYPT;
+                            break;
+                        default:
+                            prog_state = INIT;
+                            break;
+                    };
+
+                // TODO
+                // case OP_IV:
+                //     iv = buffer;
+                //     break;
+                //
+                // case OP_KEY:
+                //     key = buffer;
+                //     break;
+                //
+                // case OP_DECRYPT:
+                //     cbc_mode();
+                //     break;
+                //
+                // case OP_ENCRYPT:
+                //     cbc_mode();
+                //     break;
+
+                default:
+                    prog_state = INIT;
+                    prog_operation = OP_INIT;
+                    break;
+
+            }
+
+        }
+
+    }
+
+}
 
 /* -------------------------------------------------------------------------- */
 /* ----- CRYPTO SECTION ----------------------------------------------------- */
@@ -136,7 +326,7 @@ int validate_hw_compatibility(const struct device *dev)
             return -1;
     }
 
-    LOG_INF("CRYPTO_CAPABILITIES : %d\n", flags);
+    // LOG_INF("CRYPTO_CAPABILITIES : %d\n", flags);
 
     cap_flags = CAP_RAW_KEY | CAP_SYNC_OPS | CAP_SEPARATE_IO_BUFS;
 
